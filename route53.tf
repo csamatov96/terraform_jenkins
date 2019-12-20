3fb1544d9ee9d5bb5f7e4f7829702fb12d5d6993
csamatov96 <csamatov96@gmail.com>
2019-12-19 18:16:44 -0600

    changes

--- route53.tf
+++ route53.tf
@@ -1,7 +1,7 @@
 #https://www.terraform.io/docs/providers/aws/r/route53_record.html
 resource "aws_route53_record" "jenkins" {
   zone_id = var.zone_id #
-  name    = "jenkins".var.domain #
+  name    = "jenkins".${var.domain} #
   type    = "A" #
   ttl     = "300" #
   records = [aws_instance.jenkins.public_ip] #
